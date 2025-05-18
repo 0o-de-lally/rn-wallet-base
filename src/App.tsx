2ff09ce9f6093bc21c195e315ea2b2ee312f51e3
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-05-18 15:34:44 -0400

    build works locally

--- src/App.tsx
+++ src/App.tsx
@@ -82,8 +82,7 @@ function App(): React.JSX.Element {
             paddingBottom: safePadding,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come
-            back to see your edits.
+            HIYA
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -91,7 +90,7 @@ function App(): React.JSX.Element {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">Read the docs to discover what to do next:</Section>
+          <Section title="Learn More">true:</Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
